efc68ff4b7db50bc23a7e2a5d298a11f4df448c0
Parth Panchal <N01569361@humber.ca>
2023-07-02 02:55:05 -0400

    modified login functionality and provided intial design for Login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import About from './components/About';
 import EmpPersonalDetailsForm from './components/EmpPersonalDetailsForm';
-import Login from './components/Login';
+import Login from './components/LoginPage';
 import Navbar from './components/Navbar';
 import EmployeeSearchTable from './components/EmployeeSearchTable';
 import './Navbar.css';
@@ -13,24 +13,84 @@ import EmpProjectDetailsForm from './components/EmpProjectDetailsForm';
 import EmpSkillsCertificationForm from './components/EmpSkillsCertificationForm';
 import Display from './redux/Display';
 
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  const handleLogin = (username, password) => {
+    // Simulating login logic with hardcoded credentials
+    if (username === 'hr' && password === 'hr') {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+      // Display an error message to the user
+    }
+  };
 
-function App() {
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    // Perform any logout logic or actions
+    // For example, clear user data from local storage, reset state, or redirect to the login page
+  };
+
   return (
     <>
-      <Navbar />
+      <Navbar isLoggedIn={isLoggedIn} handleLogout={handleLogout} />
       <Routes>
         <Route path="/" element={<EmpPersonalDetailsForm />} />
         <Route path="/emp-project-details" element={<EmpProjectDetailsForm />} />
-        <Route path="/emp-certificates-skills-form" element={<EmpSkillsCertificationForm/>}/>
+        <Route path="/emp-certificates-skills-form" element={<EmpSkillsCertificationForm />} />
         <Route path="/about" element={<About />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/resume.html" element={<resume />} />
-        <Route path="/hrview" element={<EmployeeSearchTable />} />
-        <Route path="/display" element={<Display />}   />
-        </Routes>
+        <Route
+          path="/login"
+          element={isLoggedIn ? <Navigate to="/hrview" /> : <Login handleLogin={handleLogin} />}
+        />
+        <Route
+          path="/hrview"
+          element={isLoggedIn ? <EmployeeSearchTable /> : <Navigate to="/login" />}
+        />
+        <Route path="/display" element={<Display />} />
+      </Routes>
     </>
   );
-}
+};
 
 export default App;
+
+
+
+// import React from 'react';
+// import { Routes, Route } from 'react-router-dom';
+// import './App.css';
+// import About from './components/About';
+// import EmpPersonalDetailsForm from './components/EmpPersonalDetailsForm';
+// import Login from './components/LoginPage';
+// import Navbar from './components/Navbar';
+// import EmployeeSearchTable from './components/EmployeeSearchTable';
+// import './Navbar.css';
+// import './EmpDetailsForm.css'
+// import './EmployeeSearchTable.css'
+// import EmpProjectDetailsForm from './components/EmpProjectDetailsForm';
+// import EmpSkillsCertificationForm from './components/EmpSkillsCertificationForm';
+// import Display from './redux/Display';
+
+
+
+// function App() {
+//   return (
+//     <>
+//       <Navbar />
+//       <Routes>
+//         <Route path="/" element={<EmpPersonalDetailsForm />} />
+//         <Route path="/emp-project-details" element={<EmpProjectDetailsForm />} />
+//         <Route path="/emp-certificates-skills-form" element={<EmpSkillsCertificationForm/>}/>
+//         <Route path="/about" element={<About />} />
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/resume.html" element={<resume />} />
+//         <Route path="/hrview" element={<EmployeeSearchTable />} />
+//         <Route path="/display" element={<Display />}   />
+//         </Routes>
+//     </>
+//   );
+// }
+
+// export default App;
