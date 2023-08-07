95c67336ebc670b67bcbc04424a6e9521a4fd645
Harsha <harsha01111996@gmail.com>
2023-08-07 17:18:21 -0400

    final code harsha-1

--- src/apis/ResumeFinder.js
+++ src/apis/ResumeFinder.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:8080/api/v1/employees"
+//const baseURL = "http://localhost:8080/api/v1/employees"
+
+const baseURL = process.env.NODE_ENV === "production" ?"api/v1/employees":"http://localhost:8080/api/v1/employees"
+
 export default axios.create({
     baseURL:baseURL,
 });
